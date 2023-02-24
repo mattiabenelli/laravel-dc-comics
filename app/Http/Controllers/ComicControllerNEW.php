84c453dd4d6c9576f896ca53d747facb57701f94
mattiabenelli <benellimattia0@gmail.com>
2023-02-24 18:02:31 +0100

    corretta funzione modifica

--- app/Http/Controllers/ComicControllerNEW.php
+++ app/Http/Controllers/ComicControllerNEW.php
@@ -104,18 +104,18 @@ class ComicControllerNEW extends Controller
 
         $form_data = $request->all();
 
-        $newComic = new Comic();
-        $newComic->title = $form_data['title'];
-        $newComic->description = $form_data['description'];
-        $newComic->thumb = $form_data['thumb'];
-        $newComic->price = $form_data['price'];
-        $newComic->series = $form_data['series'];
-        $newComic->sale_date = $form_data['sale_date'];
-        $newComic->type = $form_data['type'];
+        $comic->title = $form_data['title'];
+        $comic->description = $form_data['description'];
+        $comic->thumb = $form_data['thumb'];
+        $comic->price = $form_data['price'];
+        $comic->series = $form_data['series'];
+        $comic->sale_date = $form_data['sale_date'];
+        $comic->type = $form_data['type'];
 
-        $newComic->save();
 
-        $newComic->update($form_data);
+        $comic->update($form_data);
+
+        $comic->save();
 
         return redirect()->route('comics.show', ['comic' => $comic->id]);
 
