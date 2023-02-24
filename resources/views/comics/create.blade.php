c98182f0b37b3620bf2d73a79e2d4c3aa55f7ae6
mattiabenelli <benellimattia0@gmail.com>
2023-02-24 15:37:39 +0100

    salvataggio nuovo comic

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,37 +6,37 @@
             
             <div class="form-element">
                 <label>Titolo</label>
-                <input type="text" name="titolo" placeholder="Inserisci il titolo">
+                <input type="text" name="title" placeholder="Inserisci il titolo">
             </div>
 
             <div class="form-element">
                 <label>Descrizione</label>
-                <input type="text" name="descrizione" placeholder="Inserisci la descrizione">
+                <input type="text" name="description" placeholder="Inserisci la descrizione">
             </div>
 
             <div class="form-element">
                 <label>Copertina</label>
-                <input type="text" name="copertina" placeholder="Inserisci la copertina">
+                <input type="text" name="thumb" placeholder="Inserisci la copertina">
             </div>
 
             <div class="form-element">
                 <label>Prezzo</label>
-                <input type="text" name="prezzo" placeholder="Inserisci il prezzo">
+                <input type="text" name="price" placeholder="Inserisci il prezzo">
             </div>
 
             <div class="form-element">
                 <label>Serie</label>
-                <input type="text" name="serie" placeholder="Inserisci la serie a cui appartiene">
+                <input type="text" name="series" placeholder="Inserisci la serie a cui appartiene">
             </div>
             
             <div class="form-element">
                 <label>Data d'uscita</label>
-                <input type="text" name="data" placeholder="Inserisci la data d'uscita">
+                <input type="text" name="sale_date" placeholder="Inserisci la data d'uscita">
             </div>
 
             <div class="form-element">
                 <label>Tipo</label>
-                <input type="text" name="tipo" placeholder="Inserisci la tipo">
+                <input type="text" name="type" placeholder="Inserisci la tipo">
             </div>
 
             <button type="submit">Crea nuovo fumetto</button>
